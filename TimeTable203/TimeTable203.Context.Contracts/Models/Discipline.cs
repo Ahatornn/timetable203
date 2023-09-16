efe3fb01f782c001ebe223dca7084214e11023ea
Anatoly Konoplev <anatoly.konoplev@monopoly.su>
2023-09-16 16:01:51 +0300

    Подготовка проекта

---
+++ TimeTable203/TimeTable203.Context.Contracts/Models/Discipline.cs
@@ -0,0 +1,23 @@
+﻿namespace TimeTable203.Context.Contracts.Models
+{
+    /// <summary>
+    /// Предмет
+    /// </summary>
+    public class Discipline
+    {
+        /// <summary>
+        /// ИД
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
