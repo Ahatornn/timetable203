5645715842a63f71212655778665b92ff66aaa51
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-11-02 19:01:16 +0300

    отключил tracking

--- TimeTable203/TimeTable203.Api/Program.cs
+++ TimeTable203/TimeTable203.Api/Program.cs
@@ -16,14 +16,12 @@ builder.Services.AddLoggerRegistr();
 
 builder.Services.AddDependences();
 
-//builder.Services.AddLoggerRegistr();
-
 var conString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContextFactory<TimeTableContext>(
      options =>
      {
          options.UseSqlServer(conString);
-         //options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+         options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
      },
     ServiceLifetime.Scoped);
 
