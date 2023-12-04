c1eb0654a8748bae1805988188e2656d6b66dfd0
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-12-04 14:02:09 +0300

    Проверка Actions github на push при 3 нерешенных тестов

--- TimeTable203/TimeTable203.Api/Validators/Discipline/CreateDisciplineRequestValidator.cs
+++ TimeTable203/TimeTable203.Api/Validators/Discipline/CreateDisciplineRequestValidator.cs
@@ -4,12 +4,12 @@ using TimeTable203.Api.ModelsRequest.Discipline;
 namespace TimeTable203.Api.Validators.Discipline
 {
     /// <summary>
-    /// 
+    /// Валидарот класса <see cref="CreateDisciplineRequest"/>
     /// </summary>
     public class CreateDisciplineRequestValidator : AbstractValidator<CreateDisciplineRequest>
     {
         /// <summary>
-        /// 
+        /// Инициализирую <see cref="CreateDisciplineRequestValidator"/>
         /// </summary>
         public CreateDisciplineRequestValidator()
         {
